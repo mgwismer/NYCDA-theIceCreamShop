6abc91737875675b266374c6ef648429452fe763
Margaret Wismer <margaretgwismer.gmail.com>
2017-01-06 10:44:33 -0500

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
+    Order.new
   end
 
   def create
+    @user = User.create(user_params)
+    Order.create(user_id: @user.id)
+    Wishlist.create(user_id: @user.id)
+    redirect_to "/users/#{@user.id}"
   end
 
   def edit
@@ -18,5 +27,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :address, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
